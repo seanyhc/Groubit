3cf4b8598c06519037e3474911ce3c66473930ac
seanyhc <sean.yhc@gmail.com>
2011-12-28 11:45:21 +0800

    create empty iphone project

---
+++ Groubit_iPhone/Groubit_iPhoneTests/Groubit_iPhoneTests.m
@@ -0,0 +1,32 @@
+//
+//  Groubit_iPhoneTests.m
+//  Groubit_iPhoneTests
+//
+//  Created by Sean Chen on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import "Groubit_iPhoneTests.h"
+
+@implementation Groubit_iPhoneTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Groubit_iPhoneTests");
+}
+
+@end
