f4494f222d83b46f1a9866eb0618288bf799d7dd
Jeffrey <jeffffrey@gmail.com>
2011-12-28 12:18:55 +0800

    Init check-in

---
+++ Groubit_iPhone/Groubit_iPhone/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Groubit_iPhone
+//
+//  Created by Jeffrey on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Groubit_iPhone/Groubit_iPhone/TestClass.m
@@ -0,0 +1,24 @@
+//
+//  TestClass.m
+//  Groubit_iPhone
+//
+//  Created by Jeffrey on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//  From Jeffrey
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
