703e0129b8b863a689dc13006b1cffab8da6fc96
seanyhc <sean.yhc@gmail.com>
2011-12-28 11:49:39 +0800

    Revert "Revert "create empty iphone project""
    This reverts commit 9f01dddafaf4eb746fb236c0c76784c4a2986708.

---
+++ Groubit_iPhone/Groubit_iPhone/iPad/Groubit_iPhoneAppDelegate_iPad.h
@@ -0,0 +1,13 @@
+//
+//  Groubit_iPhoneAppDelegate_iPad.h
+//  Groubit_iPhone
+//
+//  Created by Sean Chen on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import "Groubit_iPhoneAppDelegate.h"
+
+@interface Groubit_iPhoneAppDelegate_iPad : Groubit_iPhoneAppDelegate
+
+@end
---
+++ Groubit_iPhone/Groubit_iPhone/iPhone/Groubit_iPhoneAppDelegate_iPhone.m
@@ -0,0 +1,13 @@
+//
+//  Groubit_iPhoneAppDelegate_iPhone.m
+//  Groubit_iPhone
+//
+//  Created by Sean Chen on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import "Groubit_iPhoneAppDelegate_iPhone.h"
+
+@implementation Groubit_iPhoneAppDelegate_iPhone
+
+@end
