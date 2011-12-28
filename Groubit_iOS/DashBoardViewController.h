785343bf4a5ca6a070f6cb4455cbe16d32145c23
seanyhc <sean.yhc@gmail.com>
2011-12-28 19:02:31 +0800

    add tab controller and 3 basic tabs
    from window based application, created tab controller and 3 table:
    dashboard, habits, friends

---
+++ Groubit_iOS/DashBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashBoardViewController.h
+//  Groubit_iOS
+//
+//  Created by Sean Chen on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashBoardViewController : UIViewController
+
+@end
---
+++ Groubit_iOS/Groubit_iOS/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Groubit_iOS
+//
+//  Created by Sean Chen on 12/28/11.
+//  Copyright 2011 UCB MIMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
