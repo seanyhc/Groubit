60b9287f20520c531a4062c6e77a2ab907046ae6
Joey Tseng <joey.hyts@gmail.com>
2012-08-16 13:17:11 +0800

    Add new talbe view version of Add Habit page.

---
+++ Groubit_iOS/Groubit_iOS/iPhone/HabitsAddTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HabitsAddTableViewController.h
+//  Groubit_iOS
+//
+//  Created by Joey Tseng on 8/16/12.
+//  Copyright (c) 2012 Groubit Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HabitsAddTableViewController : UITableViewController
+
+@end
